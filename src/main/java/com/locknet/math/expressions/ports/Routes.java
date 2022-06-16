7ddf808dfc644919d20727743d43d72c1928462e
locknet11 <locknet41@protonmail.com>
2022-06-15 23:38:19 -0300

    base64 decoding and fixes

--- src/main/java/com/locknet/math/expressions/ports/Routes.java
+++ src/main/java/com/locknet/math/expressions/ports/Routes.java
@@ -2,5 +2,5 @@ package com.locknet.math.expressions.ports;
 
 public final class Routes {
 
-	public static final String CALCULATE_URL = "/v1/calculate";
+	public static final String CALCULATE_URL = "/api/v1/calculate";
 }
--- src/main/java/com/locknet/math/expressions/ports/controllers/ExpressionController.java
+++ src/main/java/com/locknet/math/expressions/ports/controllers/ExpressionController.java
@@ -1,13 +1,13 @@
 package com.locknet.math.expressions.ports.controllers;
 
 import java.util.Optional;
-
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.locknet.math.expressions.domain.exceptions.ExpressionFormatException;
@@ -23,28 +23,23 @@ public class ExpressionController {
 
 	private final OperationService operationService;
 
-	@GetMapping
-	public ResponseEntity<?> calculate(@RequestParam(name = "expression", required = true) String expression,
+	@GetMapping()
+	@RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+	public ResponseEntity<?> calculate(@RequestParam(name = "expression", required = true) String expressionParam,
 			@RequestParam(name = "precision", required = false) Optional<Integer> precision) {
+		
+		if(Base64.isBase64(expressionParam)) {
+			byte[] decodedBytes = Base64.decodeBase64(expressionParam);
+			expressionParam = new String(decodedBytes);
+		}
 
 		try {
-			Number calculatedExpression = operationService.parseAndCalculate(expression, precision);
+			Number calculatedExpression = operationService.parseAndCalculate(expressionParam, precision);
 			return new ResponseEntity<>(calculatedExpression, HttpStatus.OK);
 		} catch (ExpressionFormatException e) {
 			return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
 		}
 	}
 
-	@PostMapping
-	public ResponseEntity<?> calculatePost(@RequestParam(name = "expression", required = true) String expression,
-			@RequestParam(name = "precision", required = false) Optional<Integer> precision) {
-
-		try {
-			Number calculatedExpression = operationService.parseAndCalculate(expression, precision);
-			return new ResponseEntity<>(calculatedExpression, HttpStatus.OK);
-		} catch (ExpressionFormatException e) {
-			return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-		}
-	}
 
 }
